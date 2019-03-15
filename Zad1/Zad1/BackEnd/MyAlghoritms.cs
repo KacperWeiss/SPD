d8cab63d73012433baf4ec5de1c0eadfdaea03c8
Kacper Weiss <kacper.weiss@gmail.com>
2019-03-15 13:31:47 +0100

    Initial commit

---
+++ Zad1/Zad1/BackEnd/MyAlghoritms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1.BackEnd
+{
+    static class MyAlghorithms
+    {
+        public static List<Task> PermuteTasks(List<Task> tasks, int low)
+        {
+
+        }
+    }
+}
---
+++ Zad1/Zad1/BackEnd/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1.BackEnd
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public int TimeSpan { get; set; }
+    }
+}
