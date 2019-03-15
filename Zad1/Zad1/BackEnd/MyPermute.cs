1533b4d35924636d97dfbf9dc158366045aab9d8
Kacper Weiss <kacper.weiss@gmail.com>
2019-03-15 13:49:27 +0100

    Name change for MyAlgorithms to MyPermute to seperate different algorithms

---
+++ Zad1/Zad1/BackEnd/MyPermute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1.BackEnd
+{
+    static class MyPermute
+    {
+        public static List<List<Task>> PermuteTasks(List<Task> tasks)
+        {
+            int high = tasks.Count - 1;
+            return PermuteTasks(tasks, 0, high);
+        }
+
+        private static List<List<Task>> PermuteTasks(List<Task> tasks, int lowI, int highIt)
+        {
+
+        }
+
+        private static void Swap(Task a, Task b)
+        {
+
+        }
+    }
+}
