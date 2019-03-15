9b03b98caaafb9198863d75f61ea221376025201
Kacper Weiss <kacper.weiss@gmail.com>
2019-03-15 14:19:19 +0100

    Finished initial version of MyPermute

--- Zad1/Zad1/BackEnd/MyPermute.cs
+++ Zad1/Zad1/BackEnd/MyPermute.cs
@@ -8,20 +8,38 @@ namespace Zad1.BackEnd
 {
     static class MyPermute
     {
+        static List<List<Task>> taskLists;
+
         public static List<List<Task>> PermuteTasks(List<Task> tasks)
         {
             int high = tasks.Count - 1;
-            return PermuteTasks(tasks, 0, high);
-        }
+            PermuteTasks(tasks, 0, high);
 
-        private static List<List<Task>> PermuteTasks(List<Task> tasks, int lowI, int highIt)
-        {
-
+            return taskLists;
         }
 
         private static void Swap(Task a, Task b)
         {
+            Task temporaryTask = a;
+            a = b;
+            b = temporaryTask;
+        }
 
+        private static void PermuteTasks(List<Task> tasks, int lowIt, int highIt)
+        {
+            if (lowIt == highIt)
+            {
+                taskLists.Add(tasks);
+            }
+            else
+            {
+                for (int i = lowIt; i <= highIt; i++)
+                {
+                    Swap(tasks[lowIt], tasks[i]);
+                    PermuteTasks(tasks, lowIt + 1, highIt);
+                    Swap(tasks[lowIt], tasks[i]);
+                }
+            }
         }
     }
 }
