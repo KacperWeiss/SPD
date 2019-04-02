3276c28229a426820effde9813c24b0adf453d85
Kacper Weiss <kacper.weiss@gmail.com>
2019-04-02 11:13:57 +0200

    Added permutation methods

--- JohnsonAndNEHAlgorithms/JohnsonAndNEHAlgorithms/BackEnd/Static Algorithms/Permutation.cs
+++ JohnsonAndNEHAlgorithms/JohnsonAndNEHAlgorithms/BackEnd/Static Algorithms/Permutation.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using JohnsonAndNEHAlgorithms.BackEnd.Components;
 
 namespace JohnsonAndNEHAlgorithms.BackEnd.Static_Algorithms
 {
     static class Permutation
     {
+        public static List<List<Task>> TaskLists { get; set; } = new List<List<Task>>();
 
+        public static List<List<Task>> PermuteTasks(List<Task> tasks)
+        {
+            int high = tasks.Count - 1;
+            PermuteTasks(tasks, 0, high);
+
+            return TaskLists;
+        }
+
+        private static void PermuteTasks(List<Task> tasks, int lowIt, int highIt)
+        {
+            if (lowIt == highIt)
+            {
+                List<Task> newList = new List<Task>(tasks.Count);
+
+                tasks.ForEach((item) =>
+                {
+                    newList.Add(new Task(item));
+                });
+                TaskLists.Add(new List<Task>(newList));
+            }
+            else
+            {
+                for (int i = lowIt; i <= highIt; i++)
+                {
+                    CustomSwaps.Swap(tasks, lowIt, i);
+                    PermuteTasks(tasks, lowIt + 1, highIt);
+                    CustomSwaps.Swap(tasks, lowIt, i);
+                }
+            }
+        }
     }
 }
