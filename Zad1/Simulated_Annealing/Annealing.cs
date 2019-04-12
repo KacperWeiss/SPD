3cf7622628fc0ca15b83297b42a3586141f269b0
Krystian_Wojakiewicz <Krysh97@gmail.com>
2019-04-12 13:32:22 +0200

    Initial Commit for Simulated Annealing. Calculating temperaure, neighbor selection, Sarted on accepting neighbors.

---
+++ Zad1/Simulated_Annealing/Annealing.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zad1.BackEnd;
+
+namespace Simulated_Annealing
+{
+    static class Annealing
+    {
+        public static List<Machine> machineList = new List<Machine>(Initializer.machineList.Count);
+        static int temperature;
+        static int Cmax = 1000000000;
+        const float coolingCoefficient = 0.9f;
+        
+        public static void Swap<T>(this List<T> list, int index1, int index2)
+        {
+            T tmp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = tmp;
+        }
+        //public static void func()
+        //{
+        //    Initializer.machineList.ForEach((item) =>
+        //    {
+        //        machineList.Add(new Machine(item));
+        //    });
+        //}
+
+        public static float updateTemperature()
+        {
+            return coolingCoefficient * temperature;
+        }    
+
+        public static int setInitialTemperature(int initialTemp)
+        {
+            return initialTemp;
+        }
+
+        public static float setCoolingCoefficient(float coolingCoefficient)
+        {
+            return coolingCoefficient;
+        }
+
+
+        public static bool acceptNeighborSolution(int currentCmax)
+        {
+            if(currentCmax > Cmax)
+            {
+                //tutaj funkcja z instrukcji
+            }
+            return true;
+        }
+
+        public static void generateNeighbor()
+        {
+            Random random = new Random();
+            int randomNeighbor = random.Next(0, machineList[0].Tasks.Count -1);
+            int randomNeighbor2 = random.Next(0, machineList[0].Tasks.Count - 1);
+
+            foreach (Machine machine in machineList)
+            {
+                Swap(machine.Tasks, randomNeighbor, randomNeighbor2);
+            }
+            for(int i = 0; i < machineList.Count -1; i++)
+            {
+                Configuration.sortPermutation(machineList[i].Tasks, machineList[i + 1].Tasks,
+                                              machineList[0].Tasks.Count, machineList[i].Tasks[0].TaskStart
+                                            );
+
+            }
+        }
+
+    }
+
+}
---
+++ Zad1/Simulated_Annealing/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Simulated_Annealing
+{
+    
+    class Program
+    {
+
+        private static void readFile(string[] args, Parser parser)
+        {
+            string filepath = "C:\\Users\\Krystian Wojakiewicz\\Desktop\\SPD\\Zad1\\Simulated_Annealing\\test.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(filepath))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        parser.data.Add(s);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+
+            //if (args.Length < 2)
+            //{
+            //    System.Console.WriteLine("No arguments were given");
+            //    System.Environment.Exit(1);
+            //}
+
+            readFile(args, parser);
+            parser.parseFile();
+            Initializer.initializeFromFile(parser.numberOfTasks, parser.numberOfMachines, parser.parsedTasks);
+            System.Console.WriteLine("number of machines: " + parser.numberOfMachines);
+            System.Console.ReadLine();
+        }
+    }
+}
